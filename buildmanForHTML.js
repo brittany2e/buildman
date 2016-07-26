18f26e46fc028e6ff0c37f01d7d245900a4ab16e
Brittany Tuohy <botuohy@gmail.com>
2016-07-26 00:21:20 -0500

    Added files for HTML version of the game. The student will get this file as is, and the student can decide which improvements to make, and it will be an exercise of creative problem solving to make those changes happen.

---
+++ buildmanForHTML.js
@@ -0,0 +1,37 @@
+var secretPhrase = "This is a secret phrase";
+var inProgressPhrase = secretPhrase.split(/[a-z]/i).join('-');
+
+
+var introduction = function() {
+    return inProgressPhrase;
+}
+
+var guessALetter = function(letter) {
+    
+    // 1. Look at each character in your secret phrase (hint: keep track of the index)
+    // 2. If the guessed letter is found in the secret phrase,
+    // 3. Then update your inProgressPhrase to have the guessed letter instead of a -
+    
+    // Write your code here
+    for (var index = 0; index < secretPhrase.length; index++) {
+        var secretLetter = secretPhrase.charAt(index).toUpperCase();
+        if ( secretLetter === letter.toUpperCase() ) {
+            var beginning = inProgressPhrase.substr(0, index);
+            var middle = letter.toUpperCase();
+            var end = inProgressPhrase.substr(index + 1, secretPhrase.length);
+            inProgressPhrase =  beginning + middle + end;
+        }
+    }
+    return inProgressPhrase;
+}
+
+var printPuzzleInProgress = function() {
+    return inProgressPhrase;
+}
+
+var isFinished = function() {
+    var isInProgress = inProgressPhrase.includes('-');
+    return !isInProgress;
+}
+
+
